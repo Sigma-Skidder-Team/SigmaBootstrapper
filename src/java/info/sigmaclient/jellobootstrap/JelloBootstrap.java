6a7392813a87aa4f0d7bd218a79ce5a9e3f8a955
Mark <86848614+MarkGG8181@users.noreply.github.com>
2024-10-17 18:07:31 +0300

    Update JelloBootstrap.java

--- src/java/info/sigmaclient/jellobootstrap/JelloBootstrap.java
+++ src/java/info/sigmaclient/jellobootstrap/JelloBootstrap.java
@@ -10,16 +10,49 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class JelloBootstrap {
 
     private String[] launchArgs;
 
+    public static void main(String[] args) {
+        new JelloBootstrap(args);
+    }
+
     public JelloBootstrap(String[] args) {
         File file = new File("SigmaJelloPrelauncher.jar");
         this.launchArgs = args;
-        this.downloadFileFromUrl("https://github.com/MarkGG8181/cloud/raw/refs/heads/main/sigma/SigmaJelloPrelauncher.jar", file);
+        this.downloadFileWithSHA1Check("https://github.com/Sigma-Skidder-Team/SigmaPrelauncher/releases/download/1.0/SigmaJelloPrelauncher.jar", file);
         this.launchPrelauncher(file);
+    }
+
+    private void downloadFileWithSHA1Check(String url, File file) {
+        try {
+            if (file.exists()) {
+                File tempFile = new File(file.getParent(), "SigmaJelloPrelauncher_tmp.jar");
+                downloadFileFromUrl(url, tempFile);
+
+                // Compare SHA-1 checksums
+                String existingFileHash = getSHA1Checksum(file);
+                String newFileHash = getSHA1Checksum(tempFile);
+
+                if (!existingFileHash.equals(newFileHash)) {
+                    Files.delete(file.toPath());
+                    Files.move(tempFile.toPath(), file.toPath());
+                    System.out.println("SigmaJelloPrelauncher.jar updated.");
+                } else {
+                    Files.delete(tempFile.toPath());
+                    System.out.println("SigmaJelloPrelauncher.jar is up to date.");
+                }
+            } else {
+                downloadFileFromUrl(url, file);
+            }
+        } catch (IOException | NoSuchAlgorithmException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     private void downloadFileFromUrl(String url, File file) {
@@ -29,17 +62,33 @@ public class JelloBootstrap {
         try {
             HttpURLConnection con = (HttpURLConnection)new URL(url).openConnection();
             try (InputStream is = con.getInputStream();
-                 FileOutputStream fos = new FileOutputStream(file);){
+                 FileOutputStream fos = new FileOutputStream(file);) {
                 byte[] buff = new byte[8192];
-                int readedLen = 0;
+                int readedLen;
                 while ((readedLen = is.read(buff)) > -1) {
                     fos.write(buff, 0, readedLen);
                 }
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    private String getSHA1Checksum(File file) throws IOException, NoSuchAlgorithmException {
+        MessageDigest sha1Digest = MessageDigest.getInstance("SHA-1");
+        try (InputStream fis = Files.newInputStream(file.toPath())) {
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                sha1Digest.update(buffer, 0, bytesRead);
+            }
+        }
+        byte[] sha1Bytes = sha1Digest.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte b : sha1Bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 
     public void launchPrelauncher(File file) {
@@ -50,10 +99,8 @@ public class JelloBootstrap {
             Method main = cl.getMethod("main", mainArgType);
             Object[] argsArray = new Object[]{this.launchArgs};
             main.invoke(null, argsArray);
-        }
-        catch (ClassNotFoundException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | MalformedURLException e) {
+        } catch (ClassNotFoundException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | MalformedURLException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
