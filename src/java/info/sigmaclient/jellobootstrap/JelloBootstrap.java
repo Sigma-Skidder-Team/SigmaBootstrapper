904d9be223594efe47bb72dae310f88e1205c2fa
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-01-26 22:05:08 +0200

    fix url

--- src/java/info/sigmaclient/jellobootstrap/JelloBootstrap.java
+++ src/java/info/sigmaclient/jellobootstrap/JelloBootstrap.java
@@ -25,7 +25,7 @@ public class JelloBootstrap {
     public JelloBootstrap(String[] args) {
         File file = new File("SigmaJelloPrelauncher.jar");
         this.launchArgs = args;
-        this.downloadFileWithSHA1Check("https://jelloprg.sigmaclient.cloud/download/prelauncher/", file);
+        this.downloadFileWithSHA1Check("https://jelloprg.sigmaclient.cloud/download/prelauncher/" + file.getName(), file);
         this.launchPrelauncher(file);
     }
 
